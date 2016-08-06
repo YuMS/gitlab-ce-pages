77a18cdb8de0e8073ad2161db966a0102f3842f3
Zader Zheng <yumaoshu@gmail.com>
2016-08-06 12:38:40 +0800

    use nodemon to only watch cname.txt in legacy mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 /bin/sed -i "s/<relative_url>/${RELATIVE_URL}/" /etc/nginx/nginx.conf
-nodemon -L --exec 'bash $GITLAB_CE_PAGES_WEBHOOK_DIR/helper/generate_sites.sh' --watch $GITLAB_CE_PAGES_CNAME_DIR &
+nodemon -L --exec 'bash $GITLAB_CE_PAGES_WEBHOOK_DIR/helper/generate_sites.sh' --watch $GITLAB_CE_PAGES_CNAME_DIR/cname.txt &
 /usr/sbin/nginx
 exec "$@"
